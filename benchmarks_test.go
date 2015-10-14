3f34fd3a6b8a582719c06685f1ee113fea207255
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-10-14 05:01:52 +0000

    wip: update benchmarks for blocks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -103,6 +103,82 @@ func BenchmarkArrayInArray(b *testing.B) {
 	}
 }
 
-// BenchmarkBasicVar        1000000              1419 ns/op              64 B/op          4 allocs/op
-// BenchmarkArray            500000              2590 ns/op             130 B/op          9 allocs/op
-// BenchmarkArrayInArray      10000            244304 ns/op             542 B/op         27 allocs/op
+func BenchmarkBasicBlock(b *testing.B) {
+	tmpl := `<h1>{{#greeting}}{{a}} {{b}}{{c.d}}{{/greeting}}</h1>`
+	data := map[string]interface{}{
+		"greeting": map[string]interface{}{
+			"a": "Hello",
+			"b": "World",
+			"c": map[string]interface{}{
+				"d": "!",
+			},
+		},
+	}
+
+	rend := &Renderable{
+		File: bytes.NewReader([]byte(tmpl)),
+		Data: data,
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		rend.File.Seek(0, 0)
+		buf.Reset()
+
+		b.StartTimer()
+
+		_, err := io.Copy(buf, rend)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkBlockWithOutsideVar(b *testing.B) {
+	tmpl := `<h1>{{#greeting}}{{a}} {{b}}{{c.d}}{{/greeting}}</h1>`
+	data := map[string]interface{}{
+		"a": "Hello",
+		"greeting": map[string]interface{}{
+			"b": "World",
+			"c": map[string]interface{}{
+				"d": "!",
+			},
+		},
+	}
+
+	rend := &Renderable{
+		File: bytes.NewReader([]byte(tmpl)),
+		Data: data,
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		rend.File.Seek(0, 0)
+		buf.Reset()
+
+		b.StartTimer()
+
+		_, err := io.Copy(buf, rend)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkBasicVar                 500000              2192 ns/op             160 B/op          8 allocs/op
+// BenchmarkArray                    500000              2965 ns/op             162 B/op         11 allocs/op
+// BenchmarkArrayInArray              10000            310256 ns/op             734 B/op         35 allocs/op
+// BenchmarkBasicBlock               200000              8368 ns/op             832 B/op         39 allocs/op
+// BenchmarkBlockWithOutsideVar      200000              9625 ns/op             880 B/op         41 allocs/op
