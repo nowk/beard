48af3ac9f7ae640f164c501837a70238718d7b72
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-10-14 04:53:14 +0000

    Rework test

--- block_test.go
+++ block_test.go
@@ -21,28 +21,29 @@ func Test_blockisFinished(t *testing.T) {
 	}
 }
 
-func Test_blockgetValueDotOnSlice(t *testing.T) {
+func Test_blockgetvofDotOnSlice(t *testing.T) {
 	bl := newBlock("", 0, []interface{}{
-		"a", "b",
+		"a",
+		"b",
 	})
 
-	var exp = []string{
-		"a", "b",
+	{
+		var exp = "a"
+
+		d := bl.getvof(".")
+		if got := d.(string); exp != got {
+			t.Errorf("expected %s, got %s", exp, got)
+		}
 	}
 
-	d := bl.getData(".")
-	if got := d.(string); exp[bl.iterd] != got {
-		t.Errorf("expected %s, got %s", exp[bl.iterd], got)
-	}
 	bl.increment()
 
-	d = bl.getData(".")
-	if got := d.(string); exp[bl.iterd] != got {
-		t.Errorf("expected %s, got %s", exp[bl.iterd], got)
-	}
-	bl.increment()
+	{
+		var exp = "b"
 
-	if !bl.isFinished() {
-		t.Errorf("expected block to be finished")
+		d := bl.getvof(".")
+		if got := d.(string); exp != got {
+			t.Errorf("expected %s, got %s", exp, got)
+		}
 	}
 }
